2a716fb350c8ff3a6dd9542f8dcb4f78578080f8
Aidan T. Manning <periodicaidan@gmail.com>
2020-10-25 20:23:00 -0500

    Connected to DB in JS

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 const mysql = require('mysql');
 
+const conn = mysql.createConnection({
+    user: 'root',
+    password: '',
+    host: 'localhost',
+    database: 'employee_manager_system'
+});
+
+function showTables(conn) {
+    conn.query('show tables', (err, rows) => {
+        if (err)
+            throw err;
+
+        console.table(rows);
+    });
+}
+
+showTables(conn);
